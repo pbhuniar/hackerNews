c1ff95e2a379d7d9c1cfc7dc22bba7e810564b05
ShubhadipBH <shubhadip.bhowmick.95@gmail.com>
2021-07-29 17:43:11 +0530

    1stAPI

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import {React,useEffect,useState} from 'react'
+import axios from 'axios'
+ 
+
 
 export default function Home() {
+   
+     
+    let [hackerNewsID,setHackerNewsData]=useState({
+        data: []
+     })
+     
+     useEffect(()=>{
+         axios.get(`https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`).
+         then(res=>{
+            console.log(res.data);
+            setHackerNewsData({data:res.data})
+         console.log(hackerNewsID);
+
+
+         }).catch(err=>{console.log(err);})
+    },[setHackerNewsData])
+ 
+    
+   
     return (
         <div>
             THis is Home !
+
+ 
+{hackerNewsID.data.map(e=>{
+
+    <li>
+        <ul>{e}</ul>
+    </li>
+
+})}
+
         </div>
     )
 }
